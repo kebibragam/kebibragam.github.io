f351b91b062f644c7866b65555201b9a6882e007
Bibek Magar <kebibragam@gmail.com>
2025-02-09 23:50:37 +0545

    first commit

---
+++ src/const/const.js
@@ -0,0 +1,150 @@
+export const experience = [
+  {
+    title: "Front-end Next",
+    corporation: "Autolab Technologies ",
+    date: "11/2024 – Present",
+  },
+  {
+    title: "Front-end React",
+    corporation: "Treeleaf Technologies",
+    date: "07/2023 – 10/2024",
+  },
+];
+
+export const IconKeys = {
+  html: "html",
+  css: "css",
+  javaScript: "javaScript",
+  react: "react",
+  astro: "astro",
+  materialUi: "materialUi",
+  nextjs: "nextjs",
+  redux: "redux",
+  styled: "styled",
+  tailwind: "tailwind",
+  radix: "radix",
+  reactQuery: "reactQuery",
+  typeScript: "typeScript",
+  zustand: "zustand",
+  git: "git",
+  github: "github",
+  stripe: "stripe",
+  shadcn: "shadcn",
+  nodejs: "nodejs",
+  mongodb: "mongodb",
+  vite: "vite",
+  // here add more icon keys
+};
+
+export const skills = [
+  {
+    title: "Html",
+    icon: IconKeys.html,
+  },
+  {
+    title: "Css",
+    icon: IconKeys.css,
+  },
+  {
+    title: "JavaScript",
+    icon: IconKeys.javaScript,
+  },
+  {
+    title: "Astro",
+    icon: IconKeys.astro,
+  },
+  {
+    title: "Tailwindcss",
+    icon: IconKeys.tailwind,
+  },
+  {
+    title: "React Js",
+    icon: IconKeys.react,
+  },
+  {
+    title: "Next Js",
+    icon: IconKeys.nextjs,
+  },
+  {
+    title: "Git",
+    icon: IconKeys.git,
+  },
+
+  {
+    title: "Github",
+    icon: IconKeys.github,
+  },
+  {
+    title: "Material UI",
+    icon: IconKeys.materialUi,
+  },
+
+  {
+    title: "Redux Js",
+    icon: IconKeys.redux,
+  },
+  {
+    title: "Styled Components",
+    icon: IconKeys.styled,
+  },
+  {
+    title: "Node Js",
+    icon: IconKeys.nodejs,
+  },
+  {
+    title: "TypeScript",
+    icon: IconKeys.typeScript,
+  },
+  {
+    title: "Radix UI",
+    icon: IconKeys.radix,
+  },
+
+  {
+    title: "React Query",
+    icon: IconKeys.reactQuery,
+  },
+  {
+    title: "Vite",
+    icon: IconKeys.vite,
+  },
+  {
+    title: "Zustand",
+    icon: IconKeys.zustand,
+  },
+
+  {
+    title: "Shadcn UI",
+    icon: IconKeys.shadcn,
+  },
+  {
+    title: "Mongo Db",
+    icon: IconKeys.mongodb,
+  },
+];
+
+export const projects = [
+  {
+    img: "../../../img/tasks-manager.webp",
+    title: "Tasks Manager",
+    link: "https://task-manager-ecru-chi.vercel.app/dashboard",
+    description: `Task manager`,
+    technologies: [
+      {
+        icon: IconKeys.typeScript,
+      },
+      {
+        icon: IconKeys.nextjs,
+      },
+      {
+        icon: IconKeys.tailwind,
+      },
+      {
+        icon: IconKeys.zustand,
+      },
+      {
+        icon: IconKeys.shadcn,
+      },
+    ],
+  },
+];
